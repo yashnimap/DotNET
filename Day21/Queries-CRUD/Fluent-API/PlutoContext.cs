ae7c2f3f83cf6f1874d4c4ae7e7a1d390b3a33d8
yash jadhav <yashjadhav@gmail.com>
2024-05-07 17:45:46 +0530

    abcd

---
+++ Day21/Queries-CRUD/Fluent-API/PlutoContext.cs
@@ -0,0 +1,50 @@
+﻿using System.Data.Entity;
+
+namespace DataAnnotations
+{
+    public class PlutoContext : DbContext
+    {
+        public PlutoContext()
+            : base("name=PlutoContext")
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>()
+                .Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            modelBuilder.Entity<Course>()
+                .Property(c => c.Description)   
+                .IsRequired()   
+                .HasMaxLength(2000);
+
+            modelBuilder.Entity<Course>()
+                .HasRequired(c => c.Author)
+                .WithMany(a => a.Courses)
+                .HasForeignKey(c => c.AuthorId)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Course>()
+                .HasMany(a => a.Tags)   
+                .WithMany(t => t.Courses)
+                .Map(m => {
+                    m.ToTable("CourseTags");
+                    m.MapLeftKey("CourseId");
+                    m.MapRightKey("TagId");
+                });
+
+            modelBuilder.Entity<Course>()
+                .HasRequired(c => c.Cover)
+                .WithRequiredPrincipal(c => c.course);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Day21/Queries-CRUD/Fluent-API/Program.cs
@@ -0,0 +1,79 @@
+﻿using DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace Queries
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //===========ADDING OBJECTS===============
+            var context = new PlutoContext();
+
+            //var authors = context.Authors.ToList();
+            //var author = context.Authors.Single(a => a.Id==1);
+            //var course = new Course {
+            //    Name = "New Course",
+            //    Description = "New Description",
+            //    FullPrice = 19.95f,
+            //    Level = 1,
+            //    Author = author,
+            //};
+
+            //var course = new Course { 
+            //    Name = "Yash",
+            //    Description = "jadhav",
+            //    FullPrice = 12.9f,
+            //    Level = 2,
+            //    Author = new Author {Id = 6, Name="yash ajdhav"}
+            //};
+
+            //context.Courses.Add(course);
+            //context.SaveChanges();
+
+            //===========UPDATING OBJECTS===============
+            //var courese = context.Courses.Find(1002);
+            //courese.Name = "New name";
+            //courese.AuthorId = 1;
+            //context.SaveChanges();
+
+            //===========REMOVE OBJECTS===============
+            //var courses = context.Courses.Find(1002);
+            //context.Courses.Remove(courses);
+            //context.SaveChanges();
+
+            //var authors = context.Authors.Find(1003);
+            //context.Authors.Remove(authors);//this will give an error because author id is map with courses
+            //context.SaveChanges();
+
+            //we can explicitly delete the above record by using eager loading
+            var author = context.Authors.Include(c =>c.Courses).Single(a => a.Id == 1003);
+            context.Courses.RemoveRange(author.Courses);
+            context.Authors.Remove(author);
+            context.SaveChanges();
+
+            //for single record
+            //var author = context.Authors
+            //                    .Include(a => a.Courses)
+            //                    .Single(a => a.Id == 1003);
+
+            //var course = author.Courses.Single(); // Assuming there's only one course per author
+
+            //Remove the course associated with the author
+            //context.Courses.Remove(course);
+
+            //Remove the author
+            //context.Authors.Remove(author);
+
+            //Save changes to the database
+            //context.SaveChanges();
+        }
+    }
+    }
+
